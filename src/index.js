6606a9b90a86ead9389cf9280e01ca05de89c900
ayushdubey7799 <ayushdubey7799@gmail.com>
2023-05-18 12:29:45 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+  // failing the test
+    return false
+    })
 
 ReactDOM.render(<App />, document.getElementById("root"));
