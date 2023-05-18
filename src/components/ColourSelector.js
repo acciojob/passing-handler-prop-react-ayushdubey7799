dce93dd8a067dd79cdc30a83ebe96dd10ef519a7
ayushdubey7799 <ayushdubey7799@gmail.com>
2023-05-18 12:03:40 +0530

    commit

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import "../styles/Child.css";
 
 const ColourSelector = (props) => {
+  // console.log(props);
   const { config, selectNextBackground } = props
   const { background } = config;
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
-      {/* label should come here */}
+    <button className={"btn-"+config.key} onClick={() => selectNextBackground({background: background})}>
+      {config.label}
     </button>
   )
 }
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,13 @@
+import React from "react";
+import { useState } from "react";
+import "../styles/Child.css";
 
+const Selection = (props) => {
+   let [selectionStyle, updateSelectionStyle] = useState({background: "orange"});
+   
+   let color = props.nextBackground;
+
+   return <div className = "fix-box" onClick={() => updateSelectionStyle(color)} style={{backgroundColor: selectionStyle.background}}>Selection</div>
+}
+
+export default Selection
